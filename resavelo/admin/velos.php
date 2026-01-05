cab26efa7744e7c593ef9fc86a10d8b4b33584d6
totorz76 <victor76.zammit@gmail.com>
2026-01-05 16:37:18 +0100

    liste vélos

--- resavelo/admin/velos.php
+++ resavelo/admin/velos.php
@@ -22,12 +22,39 @@ include PATH_PROJET . '/includes/partials/header.php';
      <?php
         if (count($veloArray) === 0) :
             echo '<h3>Aucun vélo enregistré</h3>';
-            // echo '<a href="' . WEB_ROOT . '/driver/add-driver.php" class="btn btn-secondary mb-3">Ajouter un conducteur</a>';
+            echo '<a href="' . WEB_ROOT . '/admin/velos_form.php">Ajouter un vélo</a>';
             die();
         endif;
 
         ?>
         <h1>Liste des vélos</h1>
+        <a href="<?= WEB_ROOT . './admin/velos_form.php' ?>">Ajouter un vélo</a>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                    <th>Quantité</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Date de création</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach ($veloArray as $velo) : ?>
+                    <tr>
+                        <td><?= $velo['name'] ?></td>
+                        <td><?= $velo['price'] ?>€</td>
+                        <td><?= $velo['quantity'] ?></td>
+                        <td><?= $velo['description'] ?></td>
+                        <td><img src="<?= $velo['image_url'] ?>" alt="<?= $velo['name'] ?>"></td>
+                        <td><?= $velo['created_at'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
 </body>
 </html>
