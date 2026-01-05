c4f6136785abc14158735ca462eeee2793255757
totorz76 <victor76.zammit@gmail.com>
2026-01-05 14:39:08 +0100

    init index.php

---
+++ resavelo/admin/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../config/db_connect.php';
+
+require_once __DIR__ . '/../includes/functions_velos.php';
+require_once __DIR__ . '/../includes/functions_reservation.php';
+require_once __DIR__ . '/../includes/functions_calculation.php';
+?>
+
+<a href="velos.php">Liste des vélos</a>
